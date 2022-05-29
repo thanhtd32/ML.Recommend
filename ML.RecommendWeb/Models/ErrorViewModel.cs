fd7fa1496655e92a716a98875e7a7191a0f4bfef
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2022-05-29 21:07:20 +0900

    Add files via upload

---
+++ ML.RecommendWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ML.RecommendWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
